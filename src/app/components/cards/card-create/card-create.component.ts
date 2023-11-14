be94f0e7af88b059a963371eb75621322a6aa5d9
Breno <nikineke@hotmail.com>
2023-11-14 15:11:36 +0000

    form completion and the one-way property binding function

--- src/app/components/cards/card-create/card-create.component.ts
+++ src/app/components/cards/card-create/card-create.component.ts
@@ -7,17 +7,36 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CardCreateComponent implements OnInit{
 
+  link = {
+    name: 'rede social',
+    url: 'url da rede social'
+}
+
+  card = {
+    id: 0,
+    name:'Matheus Fernandes',
+    picture: 'picture',
+    name1: 'Facebook',
+    url1: 'https://www.facebook.com/',
+    name2: 'Instagram',
+    url2: 'https://www.instagram.com/',
+    name3: 'Ikea',
+    url3: 'https://www.ikea.com/pt/pt/',
+    email: 'exemplo@e-mail.com',
+    telefone: '3353353351'
+  }
 
   constructor() {
   }
    ngOnInit():void { }
 
-     criarPensamento(): void {
+     criarCard(): void {
+      alert("criou um novo card")
 
      };
 
      cancelar(): void {
-
+       alert("cancelou a criação de um novo card")
      }
 
 
